83f1543e948815ac544d97d8bba428c3ee36898b
Nilesh Gupta <nileshg2@illinois.edu>
2019-09-02 15:35:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>MetaScape</title>
+</head>
+<body>
+  <form action = "post_comment.php" method = "POST">
+    <input type = "text" name="name" value="Your Name"><br>
+    <textarea name="comment" cols="50" rows = "2">Enter a comment</textarea>
+    <input type="submit" value="Comment">
+</form>
+</body>
+</html>
